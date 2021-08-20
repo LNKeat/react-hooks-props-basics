c03918883f02b966f38e05167817e52684a0d7ff
Laura <lnkeat@gmail.com>
2021-08-20 11:02:00 -0600

    Done.

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  const {articleText, isPublished, minutesToRead} = props
+  if (!isPublished) {
+    return null;
+  }else {
+    return (
+      <div>
+        <h1>{articleText}</h1>
+        <p>{minutesToRead}</p>
+      </div>
+    );
+  }
+  // return <div id="blog-content">{articleText}</div>;
 }
 
 export default BlogContent;
+ 
